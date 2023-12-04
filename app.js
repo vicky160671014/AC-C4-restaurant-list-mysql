bf8d55e8ab817cc41c01ad83461a1c8a871fc674
vicky160671014 <vicky160671014@gmail.com>
2023-12-04 16:26:04 +0800

    router init

--- app.js
+++ app.js
@@ -9,6 +9,41 @@ app.get('/',(req,res)=>{
   res.send('this will be restaurant list!')
 })
 
+//瀏覽所有餐廳
+app.get('/restaurants',(req,res)=>{
+  res.send('render restaurants')
+})
+
+//新增餐廳頁面
+app.get('/restaurants/new', (req, res) => {
+  res.send('add restaurants new page')
+})
+
+//操作資料新增餐廳
+app.post('/restaurants', (req, res) => {
+  res.send('render restaurants')
+})
+
+//瀏覽特定餐廳
+app.get('/restaurants/:id', (req, res) => {
+  res.send(`render restaurant: ${req.params.id}`)
+})
+
+//編輯特定餐廳頁面
+app.get('/restaurants/:id/edit',(req,res)=>{
+  res.send(`render restaurant edit page: ${req.params.id}`)
+})
+
+//送出修改餐廳頁面
+app.put('/restaurants/:id', (req, res) => {
+  res.send(`put restaurant: ${req.params.id}`)
+})
+
+//刪除餐廳
+app.delete('/restaurants/:id', (req, res) => {
+  res.send(`delete restaurant: ${req.params.id}`)
+})
+
 //listen
 app.listen(port,()=>{
   console.log(`Express is running on http://localhost:${port}`)
