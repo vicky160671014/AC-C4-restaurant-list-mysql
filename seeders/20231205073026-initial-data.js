f841796bf9135d2232c00e863926c179aa47d655
vicky160671014 <vicky160671014@gmail.com>
2023-12-05 16:27:03 +0800

    add restaurant seeder

---
+++ seeders/20231205073026-initial-data.js
@@ -0,0 +1,41 @@
+'use strict';
+const restaurantJson = require('../public/jsons/restaurant.json')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const restaurants = restaurantJson.results.map((restaurant)=>({
+      name: restaurant.name,
+      name_en: restaurant.name_en,
+      category: restaurant.category,
+      image: restaurant.image,
+      location: restaurant.location,
+      phone: restaurant.phone,
+      google_map: restaurant.google_map,
+      rating: restaurant.rating,
+      description: restaurant.description,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }))
+    await queryInterface.bulkInsert('Restaurants',restaurants, {})
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Restaurants', null, {})
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
