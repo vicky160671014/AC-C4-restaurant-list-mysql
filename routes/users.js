2bf06aedb0d733edda6e1a073ed0b88d98bea489
vicky160671014 <vicky160671014@gmail.com>
2023-12-27 15:35:59 +0800

    implement register and hashing password

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,42 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
 const db = require('../models')
-const Restaurant = db.Restaurant
+const User = db.User
 
-router.post('/',(req,res)=>{
-  res.send(req.body)
+router.post('/',(req, res, next)=>{
+  const { name, email, password, confirmPassword} = req.body
+  if( !email || !password ){
+    req.flash('error','電郵email及密碼password為必填')
+    return res.redirect('back')
+  }
+  if( password !== confirmPassword){
+    req.flash('error','密碼與驗證密碼不符')
+    return res.redirect('back')
+  }
+
+  return User.count({where: { email } })
+                  .then((rawCount)=>{
+                    if(rawCount > 0){
+                      req.flash('error','此email已註冊')
+                      return res.redirect('back')
+                    }
+
+                    return bcrypt.hash(password, 10)
+                                .then((hash)=> User.create({ email, name, password: hash }))
+                  })
+                  .then((user)=>{
+                    if(!user){
+                      res.redirect('back')
+                    }
+                    req.flash('success','註冊成功')
+                    return res.redirect('/login')
+                  })
+                  .catch((error)=>{
+                    error.errorMessage = '註冊失敗'
+                    return next(error)
+                  })
 })
 
 
