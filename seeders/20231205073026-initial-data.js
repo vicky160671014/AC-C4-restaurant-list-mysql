1396bee20e91df95650907c3d2775a5cd7f86834
vicky160671014 <vicky160671014@gmail.com>
2023-12-28 22:06:49 +0800

    modify seeder

--- seeders/20231205073026-initial-data.js
+++ seeders/20231205073026-initial-data.js
@@ -1,6 +1,12 @@
 'use strict';
-const restaurantJson = require('../public/jsons/restaurant.json')
 const bcrypt = require('bcryptjs')
+const initialRestaurantData = require('../public/jsons/restaurant.json').results
+initialRestaurantData.forEach((data)=>{
+  if(data.id <= 4) {data.userId = 1}
+  else {data.userId =2}
+  data.createdAt = new Date()
+  data.updatedAt = new Date()
+  })
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -8,35 +14,25 @@ module.exports = {
     let transaction
     try {
       transaction = await queryInterface.sequelize.transaction()
-      const restaurants = restaurantJson.results.map((restaurant)=>({
-        name: restaurant.name,
-        name_en: restaurant.name_en,
-        category: restaurant.category,
-        image: restaurant.image,
-        location: restaurant.location,
-        phone: restaurant.phone,
-        google_map: restaurant.google_map,
-        rating: restaurant.rating,
-        description: restaurant.description,
-        userId:1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      }))
-      const salt = await bcrypt.genSalt(10)
-      const hash = await bcrypt.hash('password',salt)
-      await queryInterface.bulkInsert('Users',[
-        {
-        id:1,
-        name:'user1',
-        email:'user1@example.com',
-        password: hash,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ],{transaction})
 
-      await queryInterface.bulkInsert('Restaurants',restaurants, {transaction})
+      const initialUsers = await Promise.all(Array.from({ length:2 }, async(_,i)=>{
+        const hash = await bcrypt.hash('12345678',10)
+
+        return {
+          id: i+1,
+          name: `user${ i+1 }`,
+          email: `user${ i+1 }@example.com`,
+          password: hash,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      }))
+
+      await queryInterface.bulkInsert('Users',initialUsers,{ transaction })
+      await queryInterface.bulkInsert('Restaurants',initialRestaurantData,{ transaction })
+
       await transaction.commit()
+
     } catch (error) {
       if(transaction) await transaction.rollback()
     }
